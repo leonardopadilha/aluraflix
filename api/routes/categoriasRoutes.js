6da84a49076bb4053eedd98a5e06bd5149a456b7
leonardopadilha <leonardopadilha28@gmail.com>
2022-12-28 18:20:54 -0300

    Incluindo métodos para: Apagar e Alterar Categorias

--- api/routes/categoriasRoutes.js
+++ api/routes/categoriasRoutes.js
@@ -6,4 +6,7 @@ const router = Router()
 module.exports = router
     .get('/categorias', CategoriaController.pegaTodasCategorias)
     .get('/categorias/:id', CategoriaController.pegaCategoriaPorId)
+    .get('/categorias/tipo/:titulo', CategoriaController.pegaCategoriaPorTipo)
     .post('/categorias', CategoriaController.salvaCategoria)
+    .put('/categorias/:id', CategoriaController.alteraCategoria)
+    .delete('/categorias', CategoriaController.apagaCategoria)
