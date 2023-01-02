98403981d98b9e8d5f85073fde10b71457d08745
leonardopadilha <leonardopadilha28@gmail.com>
2023-01-02 09:27:17 -0300

    Adicionando novas rotas

--- api/routes/videosRoutes.js
+++ api/routes/videosRoutes.js
@@ -4,10 +4,12 @@ const VideoController = require('../controllers/VideoController')
 const router = Router()
 
 module.exports = router
-    .get('/videos', VideoController.pegaTodosVideos)
+    .get('/videos/query', VideoController.pegaTodosVideos)
+    .get('/videos', VideoController.exibeTodosVideos)
     .get('/videos/:id', VideoController.pegaVideoId)
     .get('/videos/:videoId/categorias/:categoriaId', VideoController.pegaVideoPorCategoria)
     .post('/videos', VideoController.salvaVideo)
+    .post('/videos', VideoController.salvarVariosVideos)
     .put('/videos/:id', VideoController.alteraVideo)
     .delete('/videos/:id', VideoController.deletaVideo)
     .delete('/videos', VideoController.deletaVideosMassa)
